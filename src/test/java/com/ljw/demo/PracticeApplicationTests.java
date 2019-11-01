1498caf518d842fca045028793fcae31e7fb59cc
lijw <lijw@fxqifu.com>
2019-11-01 16:03:07 +0800

    创建develop分支

---
+++ src/test/java/com/ljw/demo/PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ljw.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
