2fb1d9fcb527802687602e1bbcc7b607712ef325
lijw <lijw@fxqifu.com>
2019-11-29 10:01:18 +0800

    gitignore

--- service-feign/src/main/java/com/ljw/feign/HiController.java
+++ service-feign/src/main/java/com/ljw/feign/HiController.java
@@ -1,20 +1,88 @@
 package com.ljw.feign;
 
+import com.alibaba.fastjson.JSONObject;
 import com.ljw.feign.service.SchedualServiceHi;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @RestController
 public class HiController {
 
-    @Resource
-    private SchedualServiceHi schedualServiceHi;
+    @Resource private SchedualServiceHi schedualServiceHi;
 
     @GetMapping(value = "hi")
-    public String hi(@RequestParam String name){
+    public String hi(@RequestParam String name) {
         return schedualServiceHi.sayHiFromOne(name);
+    }
+
+    public static void main(String[] args) {
+        //        smsTemp();
+        System.out.println(String.format("%0" + 10 + "d", 55));
+        System.out.println(String.format("%010d", 55));
+        System.out.println(String.format("%0" + 10 + "d", 55).length());
+    }
+
+    private static void smsTemp() {
+        String s = "士大夫{{code}}对方是否，人口从{{message}}俄罗{{aA}}斯怕";
+//        String waitSetJsonStr = "{\"code\":5163187,\"message\":\"某个\"}";
+        String waitSetJsonStr = "";
+        String defaultStr = "{\"code\":\"123456\",\"message\":\"22\",\"aA\":\"ddd\"}";
+        String reg = "\\{\\{[a-zA-Z]+}}";
+        String reg2 = "\\{\\{.*?}}";
+
+        if (matchCount(s, reg) != matchCount(s, reg2)) {
+            System.out.println("格式不正确");
+        }
+
+        JSONObject waitSetJSON = JSONObject.parseObject(waitSetJsonStr);
+        JSONObject defaultJSON = JSONObject.parseObject(defaultStr);
+        List<String> matchStrs = listMatches(s, reg);
+        StringBuilder replaceStr = new StringBuilder();
+        for (String sm : matchStrs) {
+            String m = sm.replaceAll("\\{\\{", "").replaceAll("}}", "");
+
+            Object o = null;
+            if (null != waitSetJSON) {
+                o = waitSetJSON.get(m);
+            }
+            if (null == o) {
+                replaceStr.append(defaultJSON.get(m));
+            } else {
+                replaceStr.append(o);
+            }
+            s = s.replaceAll("\\{\\{" + m + "}}", replaceStr.toString());
+            replaceStr.setLength(0);
+        }
+        System.out.println(s);
+    }
+
+    private static int matchCount(String s, String reg) {
+        Pattern pattern = Pattern.compile(reg);
+        Matcher matcher = pattern.matcher(s);
+        int count = 0;
+        while (matcher.find()) {
+            count++;
+        }
+        return count;
+    }
+
+    private static List<String> listMatches(String s, String reg) {
+        List<String> strList = new ArrayList<>();
+        if (StringUtils.isNotBlank(s)) {
+            Pattern pattern = Pattern.compile(reg);
+            Matcher matcher = pattern.matcher(s);
+            while (matcher.find()) {
+                strList.add(matcher.group());
+            }
+        }
+        return strList;
     }
 }
--- service-feign/src/test/java/com/ljw/feign/FeignApplicationTests.java
+++ service-feign/src/test/java/com/ljw/feign/FeignApplicationTests.java
@@ -1,13 +1,35 @@
-//package com.ljw.feign;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class FeignApplicationTests {
-//
-//    @Test
-//    void contextLoads() {
-//    }
-//
-//}
+package com.ljw.feign;
+
+import com.google.common.base.Splitter;
+import com.google.common.collect.Maps;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FeignApplicationTests {
+
+    @Test
+    public void contextLoads() {
+        Optional<String> fsf = Optional.of("fsf");
+        System.out.println(fsf.isPresent());
+        System.out.println(fsf.get());
+        System.out.println(fsf);
+    }
+
+    @Test
+    public void testGuava() {
+        String s = "1,2,3,4,5";
+        List<String> list = Splitter.on(',').splitToList(s);
+        System.out.println(list);
+
+        HashMap<Object, Object> objectObjectHashMap = Maps.newHashMap();
+    }
+
+}
